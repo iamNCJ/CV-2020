ebf6c6b239c3a5579cbe216c1d877a1eed12ff4b
NCJ <iamncj@outlook.com>
2020-12-11 02:34:42 +0800

    circle stash

--- lab2/main.py
+++ lab2/main.py
@@ -3,6 +3,7 @@ import matplotlib.lines as mlines
 import matplotlib.pyplot as plt
 import numpy as np
 from tqdm import tqdm
+from matplotlib.patches import Circle
 
 
 def line_detection(img, num_rhos=360, num_thetas=360, threshold=0.5, min_count=0):
@@ -90,7 +91,7 @@ def line_detection(img, num_rhos=360, num_thetas=360, threshold=0.5, min_count=0
     plt.show()
 
 
-def circle_detection(img, threshold=0.5, min_count=0):
+def circle_detection(img, threshold=0.5, min_count=0, min_r=10, max_r=100):
     def _debug():
         if debug:
             cv2.imshow('res', edge_image)
@@ -98,25 +99,27 @@ def circle_detection(img, threshold=0.5, min_count=0):
     print("Preprocessing image...")
     edge_image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     _debug()
-    edge_image = cv2.bilateralFilter(edge_image, 90, 75, 150)
+    # edge_image = cv2.bilateralFilter(edge_image, 90, 75, 150)
+    # _debug()
+    # # edge_image = cv2.Canny(edge_image, 50, 150, apertureSize=3, L2gradient=True)
+    # # _debug()
+    # edge_image = cv2.dilate(edge_image, cv2.getStructuringElement(cv2.MORPH_RECT, (5, 5)), iterations=3)
+    # _debug()
+    # edge_image = cv2.erode(edge_image, cv2.getStructuringElement(cv2.MORPH_RECT, (5, 5)), iterations=3)
     _debug()
     edge_image = cv2.Canny(edge_image, 50, 150, apertureSize=3, L2gradient=True)
     _debug()
-    edge_image = cv2.dilate(edge_image, cv2.getStructuringElement(cv2.MORPH_RECT, (5, 5)), iterations=3)
-    _debug()
-    edge_image = cv2.erode(edge_image, cv2.getStructuringElement(cv2.MORPH_RECT, (5, 5)), iterations=3)
-    _debug()
-    edge_image = cv2.Canny(edge_image, 50, 150, apertureSize=3, L2gradient=True)
-    _debug()
-    edge_image = cv2.dilate(edge_image, cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3)), iterations=1)
-    edge_image = cv2.erode(edge_image, cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3)), iterations=1)
-    _debug()
+    # edge_image = cv2.dilate(edge_image, cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3)), iterations=1)
+    # edge_image = cv2.erode(edge_image, cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3)), iterations=1)
+    # _debug()
 
     # Quantify parameter space
     height, width = edge_image.shape[:2]
     grad_x = cv2.Scharr(edge_image, cv2.CV_32F, 1, 0)
     grad_y = cv2.Scharr(edge_image, cv2.CV_32F, 0, 1)
-    _, angle = cv2.cartToPolar(grad_x, grad_y)
+    _, angle_deg = cv2.cartToPolar(grad_x, grad_y, angleInDegrees=True)
+    angle_deg = (270 - angle_deg % 180) % 180
+    angle = np.deg2rad(angle_deg)
     # (a, b): circle center
     _as = np.arange(0, width)
     _bs = np.arange(0, height)
@@ -127,19 +130,40 @@ def circle_detection(img, threshold=0.5, min_count=0):
     # angles = [angle[tuple(x)] for x in edge_points]
     tans = np.tan(angle[edge_points.T[0], edge_points.T[1]])
     # b = a * tan(theta) - x * tan(theta) + y
-    b_values = np.matmul(np.append(_as.reshape((_as.shape[0], 1)), np.ones((_as.shape[0], 1)), axis=1),
-                         np.vstack((tans.reshape(1, tans.shape[0]), (-edge_points.T[0] * tans + edge_points.T[1]).reshape(tans.shape[0])))).transpose()
-
-    # b_values = np.matmul(edge_points, np.array([sin_thetas, cos_thetas]))
-    # filling the accumulator with np.histogram2d()
-    accumulator, _, _ = np.histogram2d(np.tile(_as, b_values.shape[0]), b_values.reshape(-1), bins=[_as, _bs])
+    accumulator = np.zeros((width, height))
+    for (y, x), tan in tqdm(zip(edge_points, tans)):
+        # print(x, y)
+        # bs = _as * tan - x * tan + y
+        # bs = bs.astype(int)
+        # b_ok = np.argwhere(0 <= bs < height)
+        for a in _as:
+            b = a * tan - x * tan + y
+            if 0 <= b < height:
+                accumulator[a, int(b)] += 1
+        # accumulator[]
+        # accumulator += 1
+    # b_values = np.matmul(np.append(_as.reshape((_as.shape[0], 1)), np.ones((_as.shape[0], 1)), axis=1),
+    #                      np.vstack((tans.reshape(1, tans.shape[0]), (-edge_points.T[0] * tans + edge_points.T[1]).reshape(tans.shape[0])))).transpose()
+    #
+    # b_values = b_values.astype(int)
+    # # b_values = np.matmul(edge_points, np.array([sin_thetas, cos_thetas]))
+    # # filling the accumulator with np.histogram2d()
+    # accumulator, _, _ = np.histogram2d(np.tile(_as, b_values.shape[0]), b_values.reshape(-1), bins=[_as, _bs])
     auto_threshold = np.max([min_count, np.max(accumulator) * threshold])
 
     # Results
     # pick up parameters which counts are large enough
     centers = np.argwhere(accumulator > auto_threshold)
-    a_idx, b_idx = centers[:, 0], centers[:, 1]
-    a, b = _as[a_idx], _bs[b_idx]
+    real_centers = []
+    for a, b in centers:
+        accumulator = np.zeros(max_r)
+        for y, x in edge_points:
+            distance = int(np.sqrt(np.square(x - a) + np.square(y - b)))
+            if min_r <= distance < max_r:
+                accumulator[distance] += 1
+
+    # a, b = centers[:, 0], centers[:, 1]
+    # a, b = _as[a_idx], _bs[b_idx]
 
     # Visualization
     # 4 subplots
@@ -154,16 +178,23 @@ def circle_detection(img, threshold=0.5, min_count=0):
     fig[1, 0].set_facecolor((0, 0, 0))
     fig[1, 0].set_title("Hough Space")
     fig[1, 0].invert_yaxis()
-    fig[1, 1].imshow(img)
+    # fig[1, 1].imshow(img)
+    fig[1, 1].imshow(angle_deg, cmap='hot', interpolation='nearest')
     fig[1, 1].set_title("Detected Circles")
     fig[1, 1].axis('off')
     # plot parameters
-    for _b in tqdm(b_values):
-        fig[1, 0].plot(_as, _b, color="white", alpha=0.01)
+    fig[1, 0].imshow(accumulator.transpose(), cmap='hot', interpolation='nearest')
+    # for _b in tqdm(b_values):
+    #     fig[1, 0].plot(_as, _b, color="white", alpha=0.01)
     # plot selected parameters
-    fig[1, 0].plot([a], [b], marker='o', color='yellow')
+    fig[0, 0].plot(real_centers[:, 0], real_centers[:, 1], marker='o', color='yellow')
+    for a, b, r in real_centers:
+        fig[0, 0].add_patch(Circle((a, b), r))
     # plot lines
-    fig[1, 1].plot([a], [b], marker='o', color='yellow')
+    # fig[1, 1].plot([a], [b], marker='o', color='yellow')
+    # for a, b in zip(range(width), range(height)):
+    #     cv2.drawMarker(img, (a, b), (int(angle[a, b] / 6.29 * 255), 0, 0))
+    # fig[1, 1].imshow(img)
     # for _a, _b in zip(a, b):
     #     cos_theta = np.cos(np.deg2rad(_theta))
     #     sin_theta = np.sin(np.deg2rad(_theta))
@@ -175,7 +206,7 @@ def circle_detection(img, threshold=0.5, min_count=0):
     #     x2 = int(x0 + diagonal * sin_theta)
     #     y2 = int(y0 - diagonal * cos_theta)
     #     # plot line
-    #     fig[1, 1].add_line(mlines.Line2D([x1, x2], [y1, y2]))
+        fig[1, 1].add_line(mlines.Line2D([x1, x2], [y1, y2]))
     plt.show()
 
 
@@ -184,4 +215,4 @@ if __name__ == "__main__":
     # line_detection(cv2.imread(f"assets/sample-1.jpg"), threshold=0.60)
     # line_detection(cv2.imread(f"assets/sample-2.jpg"), num_rhos=720, num_thetas=720, threshold=0.30)
     # line_detection(cv2.imread(f"assets/sample-3.jpg"), min_count=200)
-    circle_detection(cv2.imread(f"assets/sample-3.jpg"), threshold=0.50)
+    circle_detection(cv2.imread(f"assets/sample-6.jpg"), threshold=0.60)
--- lab2/origin.py
+++ lab2/origin.py
@@ -1,56 +1,19 @@
-# import cv2
-# import numpy as np
-#
-# img = cv2.imread('assets/sample-2.jpg')
-# gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-# edges = cv2.Canny(gray,50,150,apertureSize = 3)
-#
-# lines = cv2.HoughLines(edges,1,np.pi/180,50)
-# for rho,theta in lines[0]:
-#     a = np.cos(theta)
-#     b = np.sin(theta)
-#     x0 = a*rho
-#     y0 = b*rho
-#     x1 = int(x0 + 1000*(-b))
-#     y1 = int(y0 + 1000*(a))
-#     x2 = int(x0 - 1000*(-b))
-#     y2 = int(y0 - 1000*(a))
-#
-#     cv2.line(img,(x1,y1),(x2,y2),(0,0,255),2)
-#
-# cv2.imshow('res',img)
-# cv2.waitKey(-1)
+import cv2
+import numpy as np
 
-import cv2 as cv
-#拉普拉斯算子
+img = cv2.imread('assets/sample-3.jpg',0)
+img = cv2.medianBlur(img,5)
+cimg = cv2.cvtColor(img,cv2.COLOR_GRAY2BGR)
 
-src = cv.imread('assets/sample-1.jpg')
-cv.namedWindow('input_image', cv.WINDOW_NORMAL) #设置为WINDOW_NORMAL可以任意缩放
-# cv.imshow('input_image', src)
-# dx, dy = cv.spatialGradient(src)
-def _debug():
-    pass
-    # cv.imshow('res', edge_image)
-    # cv.waitKey(-1)
+circles = cv2.HoughCircles(img, cv2.HOUGH_GRADIENT, 1, 20, param1=100, param2=100)
 
-edge_image = cv.cvtColor(src, cv.COLOR_BGR2GRAY)
-_debug()
-edge_image = cv.bilateralFilter(edge_image, 90, 75, 150)
-_debug()
-edge_image = cv.Canny(edge_image, 50, 150, apertureSize=3, L2gradient=True)
-_debug()
-edge_image = cv.dilate(edge_image, cv.getStructuringElement(cv.MORPH_RECT, (5, 5)), iterations=3)
-_debug()
-edge_image = cv.erode(edge_image, cv.getStructuringElement(cv.MORPH_RECT, (5, 5)), iterations=3)
-_debug()
-edge_image = cv.Canny(edge_image, 50, 150, apertureSize=3, L2gradient=True)
-_debug()
-edge_image = cv.dilate(edge_image, cv.getStructuringElement(cv.MORPH_RECT, (3, 3)), iterations=1)
-edge_image = cv.erode(edge_image, cv.getStructuringElement(cv.MORPH_RECT, (3, 3)), iterations=1)
-_debug()
-grad_x = cv.Scharr(edge_image, cv.CV_32F, 1, 0)
-grad_y = cv.Scharr(edge_image, cv.CV_32F, 0, 1)
-magnitude, angle = cv.cartToPolar(grad_x, grad_y)
-cv.imshow("gradient", angle)
-cv.waitKey(-1)
-cv.destroyAllWindows()
+circles = np.uint16(np.around(circles))
+for i in circles[0,:]:
+    # draw the outer circle
+    cv2.circle(cimg,(i[0],i[1]),i[2],(0,255,0),2)
+    # draw the center of the circle
+    cv2.circle(cimg,(i[0],i[1]),2,(0,0,255),3)
+
+cv2.imshow('detected circles',cimg)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
