385bcef7f95198216f2d9d5c632ee427308028ad
NCJ <iamncj@outlook.com>
2021-01-13 21:29:16 +0800

    Update CNN.py

--- lab5/CNN.py
+++ lab5/CNN.py
@@ -33,9 +33,7 @@ class VGGNet(pl.LightningModule):
     def __init__(self, learning_rate=1e-3):
         super().__init__()
         self.learning_rate = learning_rate
-        self.features = make_layers(
-            [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 256, 'M', 512, 512, 512, 512, 'M', 512, 512, 512, 512, 'M'],
-            True)  # VGG19
+        self.features = make_layers([64, 'M', 128, 'M', 256, 'M', 512, 'M', 512, 512, 'M'])  #VGG8
         self.avgpool = nn.AdaptiveAvgPool2d((7, 7))
         self.classifier = nn.Sequential(
             nn.Linear(512 * 7 * 7, 4096),
